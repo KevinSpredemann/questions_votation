2d746ecd7510bbdee41efc39071ca9110e11d82d
Kevin Ruan Spredemann <kevinspredemann50@gmail.com>
2026-04-20 21:51:01 -0300

    HEY-2 being able to see the list the questions

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -6,6 +6,13 @@ use Database\Factories\QuestionFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $question
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ */
+
 class Question extends Model
 {
     /** @use HasFactory<QuestionFactory> */
---
+++ resources/views/components/question.blade.php
@@ -0,0 +1,5 @@
+@props(['question'])
+
+<div class="rounded dark:bg-slate-800/50 dark:text-slate-400 shadow shadow-blue-500/50 p-3">
+    {{ $question->question }}
+</div>
