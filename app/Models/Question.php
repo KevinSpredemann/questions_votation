903a9276fbad7ce2938e4b18335263cbe1d29343
Kevin Ruan Spredemann <kevinspredemann50@gmail.com>
2026-04-21 22:16:16 -0300

    HEY-2 handling N+1 issue

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Database\Factories\QuestionFactory;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -25,24 +24,5 @@ class Question extends Model
     public function votes(): HasMany
     {
         return $this->hasMany(Vote::class);
-    }
-
-
-    /**
-     * Summary of likes
-     * @return Attribute<float|int|numeric-string, never>
-     */
-    public function likes(): Attribute
-    {
-        return Attribute::get(fn () => $this->votes()->sum('like')) ;
-    }
-
-    /**
-    * Summary of unlikes
-    * @return Attribute<float|int|numeric-string, never>
-    */
-    public function unlikes(): Attribute
-    {
-        return Attribute::get(fn () => $this->votes()->sum('unlike')) ;
     }
 }
--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -8,7 +8,7 @@
             <button class="flex items-start space-x-1"><x-icons.thumbs-up
                     class="w-5 h-5 text-green-500 hover:text-green-300 cursor-pointer" />
                 <span class="text-green-500">
-                    {{ $question->likes }}
+                    {{ $question->votes_sum_like ?? 0 }}
                 </span>
             </button>
         </x-form>
@@ -16,7 +16,7 @@
             <button class="flex items-start space-x-1"><x-icons.thumbs-down
                     class="w-5 h-5 text-red-500 hover:text-red-300 cursor-pointer" />
                 <span class="text-red-500">
-                    {{ $question->unlikes }}
+                    {{ $question->votes_sum_unlike ?? 0 }}
                 </span>
             </button>
         </x-form>
