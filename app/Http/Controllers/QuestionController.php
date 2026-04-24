eec8c4cbb41e0bd8d731874ea6f6715a9690b08b
Kevin Ruan Spredemann <kevinspredemann50@gmail.com>
2026-04-24 15:27:04 -0300

    HEY-2 it should be able to open a question edit

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -47,4 +47,8 @@ class QuestionController extends Controller
 
         return back();
     }
+
+    public function edit(Question $question): void
+    {
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/question', [QuestionController::class, 'index'])->name('questions.index');
     Route::post('/question/store', [QuestionController::class, 'store'])->name('questions.store');
     Route::delete('/question/{question}', [QuestionController::class, 'destroy'])->name('questions.destroy');
+    Route::get('/question/{question}/edit', [QuestionController::class, 'edit'])->name('question.edit');
     Route::post('/question/like/{question}', Question\LikeController::class)->name('question.like');
     Route::post('/question/unlike/{question}', Question\UnlikeController::class)->name('question.unlike');
     Route::put('/question/publish/{question}', Question\PublishController::class)->name('questions.publish');
