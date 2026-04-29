c73267d744bef52a731d42e5de07968588ca77bb
Kevin Ruan Spredemann <kevinspredemann50@gmail.com>
2026-04-28 23:11:58 -0300

    HEY-2 ordering based on likes/unlikes

--- tests/Feature/MyQuestionsTest.php
+++ tests/Feature/MyQuestionsTest.php
@@ -3,7 +3,7 @@
 use App\Models\{Question, User};
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
-use function Pest\Laravel\{actingAs, get};
+use function Pest\Laravel\{actingAs, get, withoutExceptionHandling};
 
 it("should be able to list all questions created by me", function () {
 
@@ -35,5 +35,26 @@ it("should paginate the result", function () {
     get(route('dashboard'))
         ->assertViewHas('questions', function ($value) {
             return $value instanceof LengthAwarePaginator;
+        });
+});
+
+it("should order my like and unlike, most like question should be at the top, most unlike question should be at the bottom", function () {
+    $user       = User::factory()->create();
+    $secondUser = User::factory()->create();
+    Question::factory()->count(5)->create();
+    $mostLikedQuestion   = Question::find(3);
+    $mostUnlikedQuestion = Question::find(1);
+    $user->like($mostLikedQuestion);
+    $secondUser->unlike($mostUnlikedQuestion);
+
+    withoutExceptionHandling();
+    actingAs($user);
+
+    get(route('dashboard'))
+        ->assertViewHas('questions', function ($questions) {
+            expect($questions->first()->id)->toBe(3)
+                ->and($questions->last()->id)->toBe(1);
+
+            return true;
         });
 });
